7e44d3e5f596d8a103d5ac41e9ea95142fa6d682
“DonaldCen” <1147656534@qq.com>
2020-02-19 19:31:23 +0800

    构造netty

---
+++ src/main/java/com/xuanwu/ump/HttpSimulateGate/channel/server/ServerTemplate.java
@@ -0,0 +1,14 @@
+package com.xuanwu.ump.HttpSimulateGate.channel.server;
+
+/**
+ * @Description
+ * @Author <a href="mailto:cenyingqiang@wxchina.com">yingqiang.Cen</a>
+ * @Date 2020/2/19
+ * @Version 1.0.0
+ */
+public interface ServerTemplate {
+    /**
+     * Start the server
+     */
+    public void startServer();
+}
--- src/main/java/com/xuanwu/ump/HttpSimulateGate/server/HttpSimulateGateServer.java
+++ src/main/java/com/xuanwu/ump/HttpSimulateGate/server/HttpSimulateGateServer.java
@@ -1,5 +1,9 @@
 package com.xuanwu.ump.HttpSimulateGate.server;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 /**
  * @Description
  * @Author <a href="mailto:cenyingqiang@wxchina.com">yingqiang.Cen</a>
@@ -8,6 +12,10 @@ package com.xuanwu.ump.HttpSimulateGate.server;
  */
 public class HttpSimulateGateServer {
     public static void main(String[] args) {
+        ExecutorService bossExecutor = Executors.newCachedThreadPool();
+        ExecutorService workerExecutor = Executors.newCachedThreadPool();
+
+        ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(4);
 
     }
 }
